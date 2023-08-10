13511bf610139799078511cd7cbb5bb73cbfd98b
Barath-tech <barathsundar03@gmail.com>
2023-08-10 14:20:16 +0530

    Handled errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as MenuLoader } from "./features/menu/Menu";
+import Error from "./ui/Error";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
@@ -9,9 +10,15 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: MenuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: MenuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:ordderId", element: <Order /> },
