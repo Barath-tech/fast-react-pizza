63d5d1528ca145f544090ab83031b57d78cbe115
Barath-tech <barathsundar03@gmail.com>
2023-08-10 14:04:38 +0530

    extracted menu data

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,9 +1,16 @@
 import { getMenu } from "../../services/apiRestaurant";
 import { useLoaderData } from "react-router-dom";
+import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
-  return <h1>Menu</h1>;
+  //console.log(menu);
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
