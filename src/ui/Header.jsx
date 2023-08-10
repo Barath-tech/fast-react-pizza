06a071b813ebbac07d26688e15ceb52e6dd4718f
Barath-tech <barathsundar03@gmail.com>
2023-08-10 13:44:34 +0530

    Applayout has been added

---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <Link to="/">Fast react Pizza co.</Link>
+      </header>
+      <p>Barath</p>
+    </>
+  );
+}
+
+export default Header;
