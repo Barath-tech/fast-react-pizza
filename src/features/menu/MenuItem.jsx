b7a66cfe993d0715a2c497404372d461fda2e6a1
Barath-tech <barathsundar03@gmail.com>
2023-08-13 12:07:11 +0530

    Add to cart Button feature added

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,23 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { addItem } from "../cart/CartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleAddtoCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+    //console.log(newItem.pizzaId);
+  }
 
   return (
     <li className="flex gap-4 py-2  ">
@@ -24,7 +39,13 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to Cart</Button>
+          {!soldOut ? (
+            <Button type="small" onClick={handleAddtoCart}>
+              Add to Cart
+            </Button>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </li>
